110ff3efca4f30a8cfea9b282d4f17605b390233
exit02 <95013304+exit02@users.noreply.github.com>
2022-02-02 10:33:08 +0900

    52.Reactでの実装を意識したモックに変更 import指定の誤りがあった。 その他useSateの使い方...

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
-  const [incompleteTodos, setIncompleteTodos] = useState(
-    "ああああ",
-    "いいいい"
-  );
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "あああああ",
+    "いいいいい"
+  ]);
+  const [completeTodos, setCompleteTodos] = useState(["ううううう"]);
   return (
     <>
       <div className="input-area">
@@ -17,27 +18,26 @@ export const App = () => {
         <ul>
           {incompleteTodos.map((todo) => {
             return (
-              <div className="list-row">
+              <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
                 <button>削除</button>
               </div>
             );
           })}
-          <div className="list-row">
-            <li>いいいいいい </li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
         </ul>
       </div>
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          <div className="list-row">
-            <li>ううううううう</li>
-            <button>戻す</button>
-          </div>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
