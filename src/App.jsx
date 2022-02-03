26ce449f9dab62bc765f734f705113d156204b2c
exit02 <95013304+exit02@users.noreply.github.com>
2022-02-03 09:16:22 +0900

    53.タスクの追加処理

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,38 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  // 追加ボタン
+  const [todoText, setTodoText] = useState("");
+  // 未完了のTODO
   const [incompleteTodos, setIncompleteTodos] = useState([
     "あああああ",
     "いいいいい"
   ]);
+  // 完了のTODO
   const [completeTodos, setCompleteTodos] = useState(["ううううう"]);
+
+  // todoTextの入力処理
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  // 追加ボタン処理
+  const onClickAdd = () => {
+    // 空文字は未処理
+    if (todoText === "") return;
+    // スプレッド構文で新しい配列を生成する。
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
